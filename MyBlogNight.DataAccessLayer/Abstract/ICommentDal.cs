a43b0df3d761fe19899e56b2b441dab13cff01ae
Abdulhamitkisir <hamitkisi@gmail.com>
2024-11-01 19:49:54 +0100

    1 November

---
+++ MyBlogNight.DataAccessLayer/Abstract/ICommentDal.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlogNight.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogNight.DataAccessLayer.Abstract
+{
+    public interface ICommentDal :IGenericDal<Comment>
+    {
+
+    }
+}
--- MyBlogNight.EntityLayer/Concrete/Article.cs
+++ MyBlogNight.EntityLayer/Concrete/Article.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyBlogNight.EntityLayer.Concrete
 {
-    internal class Article
+    public class Article
     {
         public int ArticleId { get; set; }
         public string Title { get; set; }
